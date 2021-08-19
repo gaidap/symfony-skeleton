e762596304573b461af6f7a85c8fffc6f3e3ec99
gaidap <pc.gaida@gmail.com>
2021-08-19 09:55:24 +0200

    Experimenting with cache.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@
     
     use App\Entity\User;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\Cache\Adapter\FilesystemAdapter;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,6 @@
          * @Route("/user/{id}", name="user")
          */
         public function index(Request $request, User $user): Response {
-            dump($user);
             return $this->render('user/index.html.twig', [
                 'controller_name' => 'UserController',
                 'users' => [$user]
@@ -24,6 +24,16 @@
          * @Route("/allUsers", name="allUsers")
          */
         public function allUsers(): Response {
+            $cache = new FilesystemAdapter();
+            $posts = $cache->getItem('database.get_posts');
+            if(!$posts->isHit()) {
+                $posts_from_db = ['post 1', 'post 2', 'post 3'];
+                dump('connected with database...');
+                $posts->set(serialize($posts_from_db));
+                $posts->expiresAfter(5);
+                $cache->save($posts);
+            }
+            dump(unserialize($posts->get()));
             $repo = $this->getDoctrine()->getRepository(User::class);
             $users = $repo->findAll();
             return $this->render('user/index.html.twig', [
